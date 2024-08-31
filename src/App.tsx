4a107fb65e806aecb7cab85faf43a2a8d52cc553
vasuparsaniya <vasuparsaniya21@gmail.com>
2024-08-31 21:54:19 +0530

    husky setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(temp);
+  const temp;
   return (
     <div className="App">
       <header className="App-header">
